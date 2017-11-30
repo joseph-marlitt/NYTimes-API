8d106de013ed2d2e8b748c581c1026ec42060c2f
Digald <nosracer_gt@sbcglobal.net>
2017-11-29 20:59:34 -0600

    working variables for url

--- NYTimes.js
+++ NYTimes.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
-    $("#button").on('click', function() {
+    $("#search-button").on('click', function() {
         // make variables for collecting information filled out inside the form
-
         // GET request
         var searchTerm = $("#serch-term").val();
+
         var begin = $("#start-year").val();
         var beginDate = begin + "0101";
+
+        var numOfResults = ;
+
         var end = $("#end-year").val();
         var endDate = end + "0101";
         var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=7b8facd071474e0eaef550125d430f18&q=" + searchTerm + "&begin_date=" + beginDate + "&end_date=" + endDate;
@@ -17,8 +20,8 @@ $(document).ready(function() {
         }).done(function(result) {
             console.log(result);
             // append response values to the appropriate HTML divs
-
-
+            
+            
 
 
 
